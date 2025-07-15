ab2f78b4a101e68655a210fe868c0b5abdd88774
mintyry <gitmintyry@gmail.com>
2025-07-14 19:42:39 -0700

    testing select to see resulting join table

---
+++ db/leetquery3.sql
@@ -0,0 +1,28 @@
+-- Table: Employee
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- | salary      | int     |
+-- | managerId   | int     |
+-- +-------------+---------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row of this table indicates the ID of an employee, their name, salary, and the ID of their manager.
+ 
+
+-- Write a solution to find the employees who earn more than their managers.
+
+-- Return the result table in any order.
+
+-- The result format is in the following example.
+
+
+
+-- note from ryan: this is a self join exanple
+
+SELECT * 
+from Employee o
+LEFT JOIN Employee t on e.managerId = t.id
+HAVING salary >
+
