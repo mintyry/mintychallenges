5849d22960318b43031d4e904b386995e2da7cdc
mintyry <gitmintyry@gmail.com>
2025-07-14 20:03:28 -0700

    refactored for efficiency using inner join

--- db/leetquery3.sql
+++ db/leetquery3.sql
@@ -20,9 +20,10 @@
 
 
 -- note from ryan: this is a self join exanple
+-- inner join wont allow nulls
 
 SELECT o.name as employee
 from Employee o
-LEFT JOIN Employee t on o.managerId = t.id
-WHERE o.managerID IS NOT NULL AND o.salary > t.salary
+JOIN Employee t on o.managerId = t.id
+WHERE o.salary > t.salary
 ;
