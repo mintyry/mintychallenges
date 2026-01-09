c0aa7ba735faa420eeccde78a582ab11627fdbb4
mintyry <gitmintyry@gmail.com>
2026-01-08 21:20:19 -0800

    added leetjs file

---
+++ db/leet.js
@@ -0,0 +1,23 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+eg: [1,5,7,9,14]
+target: 16
+
+so we wanna find indices 2 and 3
+
+var twoSum = function(nums, target) {
+
+    let nums = [1,5,7,9,14];
+    let target = 16;
+// (starting point, end point, how to increment)
+    for (let i = 0; i < nums.length; i++) {
+i+
+    }
+    
+};
+
+twoSum();
