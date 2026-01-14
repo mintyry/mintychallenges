3ed20305b2e7ea425aec9663b9195fd300bb74d0
mintyry <gitmintyry@gmail.com>
2026-01-13 17:56:40 -0800

    convert number to array

--- leetPalindrome.js
+++ leetPalindrome.js
@@ -2,10 +2,13 @@
 // eg: 121, -121, 10
 
 // i learned when using typeof, it will always return a string ("number", etc)
+// i learned that the quotes in .split('') means split every character in a string into an array
 
 var isPalindrome = function(x) {
         if (typeof x === "number") {
             // enter logic
+            let xString = x.toString();
+            let xArr = xString.split('');
         }
         else {
             return "Enter a number instead!"
