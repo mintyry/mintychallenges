bf3dd4c64db32c32a8c796f6290e28d338241007
mintyry <gitmintyry@gmail.com>
2026-01-13 17:36:13 -0800

    set if to check data type, ensure it's a number

--- leetPalindrome.js
+++ leetPalindrome.js
@@ -1,6 +1,13 @@
 // Given an integer x, return true if x is a palindrome, and false otherwise.
 // eg: 121, -121, 10
 
+// i learned when using typeof, it will always return a string ("number", etc)
+
 var isPalindrome = function(x) {
-    
+        if (typeof x === "number") {
+            // enter logic
+        }
+        else {
+            return "Enter a number instead!"
+        }
 };
