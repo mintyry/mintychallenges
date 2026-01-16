af6be83284a69d895f535fd6bc8eec1d0d0873dc
mintyry <gitmintyry@gmail.com>
2026-01-15 19:32:16 -0800

    for loop with i and j to check for palindrome

--- leetPalindrome.js
+++ leetPalindrome.js
@@ -3,14 +3,26 @@
 
 // i learned when using typeof, it will always return a string ("number", etc)
 // i learned that the quotes in .split('') means split every character in a string into an array
+// second part of for loop is condition; loop will run as long this is true
 
-var isPalindrome = function(x) {
-        if (typeof x === "number") {
-            // enter logic
-            let xString = x.toString();
-            let xArr = xString.split('');
+var isPalindrome = function (x) {
+    if (typeof x === "number") {
+        // num to string
+        let xString = x.toString();
+        // split up each character of the string and put into an array
+        let xArr = xString.split('');
+        for (let i = 0; i < xArr.length; i++) {
+            // target the last digit/char
+            let lastChar = xArr.length - 1;
+            // start at last index; run as long as j is more than or equal to 0, the first index; decrement by 1
+            for (let j = lastChar; j >= 0; j--) {
+                if (xArr[i] === xArr[j]) {
+                    return x + "is a palindrome!"
+                }
+            }
         }
-        else {
-            return "Enter a number instead!"
-        }
+    }
+    else {
+        return "Enter a number instead!"
+    }
 };
