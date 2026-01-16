b1e53aeea5556b259ed64c73de2ae754e9c6e2b9
mintyry <gitmintyry@gmail.com>
2026-01-15 19:56:03 -0800

    reset

--- leetPalindrome.js
+++ leetPalindrome.js
@@ -4,6 +4,7 @@
 // i learned when using typeof, it will always return a string ("number", etc)
 // i learned that the quotes in .split('') means split every character in a string into an array
 // second part of for loop is condition; loop will run as long this is true
+// dont need a nested loop because it will create a grid comparison; you dont need to compare one digit to every digit
 
 var isPalindrome = function (x) {
     if (typeof x === "number") {
@@ -11,16 +12,6 @@ var isPalindrome = function (x) {
         let xString = x.toString();
         // split up each character of the string and put into an array
         let xArr = xString.split('');
-        for (let i = 0; i < xArr.length; i++) {
-            // target the last digit/char
-            let lastChar = xArr.length - 1;
-            // start at last index; run as long as j is more than or equal to 0, the first index; decrement by 1
-            for (let j = lastChar; j >= 0; j--) {
-                if (xArr[i] === xArr[j]) {
-                    return x + "is a palindrome!"
-                }
-            }
-        }
     }
     else {
         return "Enter a number instead!"
