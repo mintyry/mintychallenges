1252e4e9dda758e19edfc82a4dbeb895765c08b8
mintyry <gitmintyry@gmail.com>
2026-01-27 18:43:41 -0800

    comments

--- leetRoman.js
+++ leetRoman.js
@@ -2,6 +2,15 @@
  * @param {string} s
  * @return {number}
  */
+
+// constraints
+// 1 <= s.length <= 15
+// s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+// It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+
+// use .map 
+// Problem is simpler to solve by working the string from back to front and using a map.
+
 var romanToInt = function(s) {
     
 };
