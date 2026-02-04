194d6151e0de686b84c952d89df82b21464da685
mintyry <gitmintyry@gmail.com>
2026-02-03 19:50:59 -0800

    console log variable

--- leetRoman.js
+++ leetRoman.js
@@ -18,7 +18,7 @@ var romanToInt = function (s) {
         console.log('That\'s not a Roman numeral!');
         return 'That\'s not a Roman numeral!';
     } else {
-        let I = romanNum[0];
+        let I = 'romanNum[0]';
         let V = romanNum[1];
         let X = romanNum[2];
         let L = romanNum[3];
@@ -26,7 +26,7 @@ var romanToInt = function (s) {
         let D = romanNum[5];
         let M = romanNum[6];
 
-        console.log('I');
+        console.log(I);
         // maybe map through the array and assign number instead
 
         // make array of actual numbers and assign to roman array
